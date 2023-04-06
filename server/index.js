2cce2d40879afaa44b7b651dac83a592e4528ff3
Danilych <danilprov2004@gmail.com>
2023-04-06 20:42:41 +0200

    feat(server): connect to mongoDB

--- server/index.js
+++ server/index.js
@@ -1,9 +1,38 @@
 import express from "express";
+import jwt from "jsonwebtoken";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+const config = dotenv.config();
+
+mongoose
+  .connect(process.env.MONGOOSE_CONNECTION)
+  .then(() => console.log("Connection to DB success"))
+  .catch((err) => console.log("Fail connection to db: ", err));
 
 const app = express();
 
+app.use(express.json());
+
 app.get("/", (req, res) => {
   res.send("hello world");
+});
+
+app.post("/login", (req, res) => {
+  console.log(req.body);
+
+  const token = jwt.sign(
+    {
+      email: req.body.email,
+      fullName: "Danya",
+    },
+    "qwerty"
+  );
+
+  res.json({
+    success: true,
+    token,
+  });
 });
 
 app.listen(3001, (err) => {
