5fb41aa246008137a8cea9579a7ba709016dffb7
Danilych <danilprov2004@gmail.com>
2023-04-10 17:22:53 +0200

    refactor(server): create user controller

---
+++ server/controllers/UserController.js
@@ -0,0 +1,121 @@
+import jwt from "jsonwebtoken";
+import bcrypt from "bcrypt";
+import { validationResult } from "express-validator";
+import UserModel from "../models/User.js";
+
+export const register = async (req, res) => {
+  try {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json(errors.array());
+    }
+
+    const password = req.body.password;
+    const salt = await bcrypt.genSalt(10);
+    const passwordHash = await bcrypt.hash(password, salt);
+
+    const doc = new UserModel({
+      email: req.body.email,
+      fullName: req.body.fullName,
+      avatarUrl: req.body.avatarUrl,
+      passwordHash,
+    });
+
+    const user = await doc.save();
+
+    const token = jwt.sign(
+      {
+        _id: user._id,
+      },
+      "skubidupapa",
+      {
+        expiresIn: "30d",
+      }
+    );
+
+    res.json({
+      ...user,
+      token,
+    });
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({
+      message: "Fail register, please try again",
+    });
+  }
+};
+
+export const login = async (req, res) => {
+  try {
+    const user = await UserModel.findOne({ email: req.body.email });
+
+    if (!user) {
+      return req.status(404).json({
+        message: "User not found",
+      });
+    }
+
+    const isValidPass = await bcrypt.compare(
+      req.body.password,
+      user._doc.passwordHash
+    );
+
+    if (!isValidPass) {
+      return req.status(404).json({
+        message: "Login or password is incorrect",
+      });
+    }
+
+    const token = jwt.sign(
+      {
+        _id: user._id,
+      },
+      "skubidupapa",
+      {
+        expiresIn: "30d",
+      }
+    );
+
+    res.json({
+      ...user,
+      token,
+    });
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({
+      message: "Fail login, please try again",
+    });
+  }
+};
+export const getMyProfile = async (req, res) => {
+  try {
+    const user = await UserModel.findById(req.userId);
+
+    if (!user) {
+      return res.status(404).json({
+        message: "User not found",
+      });
+    }
+
+    const token = jwt.sign(
+      {
+        _id: user._id,
+      },
+      "skubidupapa",
+      {
+        expiresIn: "30d",
+      }
+    );
+
+    res.json({
+      ...user,
+      token,
+    });
+  } catch (err) {
+    console.log(err);
+
+    return res.status(500).json({
+      message: "Not access",
+    });
+  }
+};
--- server/index.js
+++ server/index.js
@@ -1,13 +1,11 @@
 import express from "express";
-import jwt from "jsonwebtoken";
-import bcrypt from "bcrypt";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
-import { validationResult } from "express-validator";
 
 import { registerValidation } from "./validations/auth.js";
-import UserModel from "./models/User.js";
 import checkAuth from "./utils/checkAuth.js";
+
+import { register, login, getMyProfile } from "./controllers/UserController.js";
 
 const config = dotenv.config();
 
@@ -24,123 +22,11 @@ app.get("/", (req, res) => {
   res.send("hello world");
 });
 
-app.get("/my-profile", checkAuth, async (req, res) => {
-  try {
-    const user = await UserModel.findById(req.userId);
+app.get("/my-profile", checkAuth, getMyProfile);
 
-    if (!user) {
-      return res.status(404).json({
-        message: "User not found",
-      });
-    }
+app.post("/login", login);
 
-    const token = jwt.sign(
-      {
-        _id: user._id,
-      },
-      "skubidupapa",
-      {
-        expiresIn: "30d",
-      }
-    );
-
-    res.json({
-      ...user,
-      token,
-    });
-  } catch (err) {
-    console.log(err);
-
-    return res.status(500).json({
-      message: "Not access",
-    });
-  }
-});
-
-app.post("/login", async (req, res) => {
-  try {
-    const user = await UserModel.findOne({ email: req.body.email });
-
-    if (!user) {
-      return req.status(404).json({
-        message: "User not found",
-      });
-    }
-
-    const isValidPass = await bcrypt.compare(
-      req.body.password,
-      user._doc.passwordHash
-    );
-
-    if (!isValidPass) {
-      return req.status(404).json({
-        message: "Login or password is incorrect",
-      });
-    }
-
-    const token = jwt.sign(
-      {
-        _id: user._id,
-      },
-      "skubidupapa",
-      {
-        expiresIn: "30d",
-      }
-    );
-
-    res.json({
-      ...user,
-      token,
-    });
-  } catch (err) {
-    console.log(err);
-    res.status(500).json({
-      message: "Fail login, please try again",
-    });
-  }
-});
-
-app.post("/register", registerValidation, async (req, res) => {
-  try {
-    const errors = validationResult(req);
-    if (!errors.isEmpty()) {
-      return res.status(400).json(errors.array());
-    }
-
-    const password = req.body.password;
-    const salt = await bcrypt.genSalt(10);
-    const passwordHash = await bcrypt.hash(password, salt);
-
-    const doc = new UserModel({
-      email: req.body.email,
-      fullName: req.body.fullName,
-      avatarUrl: req.body.avatarUrl,
-      passwordHash,
-    });
-
-    const user = await doc.save();
-
-    const token = jwt.sign(
-      {
-        _id: user._id,
-      },
-      "skubidupapa",
-      {
-        expiresIn: "30d",
-      }
-    );
-
-    res.json({
-      ...user,
-      token,
-    });
-  } catch (err) {
-    console.log(err);
-    res.status(500).json({
-      message: "Fail register, please try again",
-    });
-  }
-});
+app.post("/register", registerValidation, register);
 
 app.listen(3001, (err) => {
   if (err) return console.log(err);
