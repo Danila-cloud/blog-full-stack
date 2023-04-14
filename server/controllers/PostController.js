dc5455ab212243c81b57b5eb3ac0356ff0f68b4e
Danylo Provilskyi <65830545+Danila-cloud@users.noreply.github.com>
2023-04-14 17:03:37 +0200

    feat(server): implement RUD for posts (#9)

--- server/controllers/PostController.js
+++ server/controllers/PostController.js
@@ -15,8 +15,104 @@ export const create = async (req, res) => {
     res.json(post);
   } catch (error) {
     console.log(error);
+
     res.status(500).json({
       message: "Fail create post",
+    });
+  }
+};
+
+export const getAll = async (req, res) => {
+  try {
+    const posts = await PostModel.find().populate("user").exec();
+
+    res.json(posts);
+  } catch (err) {
+    res.status(500).json({
+      message: "unable get posts",
+    });
+  }
+};
+
+export const getOne = async (req, res) => {
+  try {
+    const postId = req.params.id;
+
+    PostModel.findOneAndUpdate(
+      {
+        _id: postId,
+      },
+      {
+        $inc: { viewsCount: 1 },
+      },
+      {
+        returnDocument: "after",
+      }
+    ).then((doc) => {
+      if (!doc) {
+        return res.status(404).json({
+          message: "post not found",
+        });
+      }
+
+      res.json(doc);
+    });
+  } catch (err) {
+    console.log(err);
+
+    res.status(500).json({
+      message: "unable get posts",
+    });
+  }
+};
+
+export const remove = (req, res) => {
+  try {
+    const postId = req.params.id;
+
+    PostModel.findOneAndDelete({
+      _id: postId,
+    }).then((err) => {
+      console.log(err);
+
+      res.json({
+        success: true,
+      });
+    });
+  } catch (err) {
+    console.log(err);
+
+    res.status(500).json({
+      message: "unable get posts",
+    });
+  }
+};
+
+export const update = async (req, res) => {
+  try {
+    const postId = req.params.id;
+
+    await PostModel.updateOne(
+      {
+        _id: postId,
+      },
+      {
+        title: req.body.title,
+        text: req.body.text,
+        imageUrl: req.body.imageUrl,
+        tags: req.body.tags,
+        user: req.userId,
+      }
+    );
+
+    res.json({
+      success: true,
+    });
+  } catch (err) {
+    console.log(err);
+
+    res.status(500).json({
+      message: "Unable update the post",
     });
   }
 };
