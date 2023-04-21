c401669871b0ad44bc7984a654d373ff2fd2921e
Danilych <danilprov2004@gmail.com>
2023-04-21 16:52:43 +0200

    feat(client): change favicon

--- client/app/root.tsx
+++ client/app/root.tsx
@@ -15,9 +15,58 @@ import Footer from "./components/footer";
 import { Provider } from "react-redux";
 import store from "./redux/store";
 
-export const links: LinksFunction = () => [
-  { rel: "stylesheet", href: stylesheet },
-];
+export const links: LinksFunction = () => {
+  return [
+    { rel: "stylesheet", href: stylesheet },
+    {
+      rel: 'manifest',
+      href: '/site.webmanifest',
+    },
+    {
+      rel: 'shortcut icon',
+      type: 'image/png',
+      sizes: '32x32',
+      href: '/favicons/favicon-32x32.png',
+    },
+    {
+      rel: 'icon',
+      type: 'image/png',
+      sizes: '16x16',
+      href: '/favicons/favicon-16x16.png',
+    },
+    {
+      rel: 'icon',
+      type: 'image/png',
+      sizes: '192x192',
+      href: '/favicons/android-chrome-192x192.png',
+    },
+    {
+      rel: 'icon',
+      type: 'image/png',
+      sizes: '256x256',
+      href: '/favicons/android-chrome-256x256.png',
+    },
+    {
+      rel: 'apple-touch-icon',
+      type: 'image/png',
+      sizes: '180x180',
+      href: '/favicons/apple-touch-icon.png',
+    },
+    {
+      rel: 'mask-icon',
+      href: '/favicons/safari-pinned-tab.svg',
+    },
+
+    {
+      rel: 'stylesheet',
+      href: 'https://fonts.googleapis.com/css2?family=Sora:wght@300;400;500;600;700&display=swap',
+    },
+    {
+      rel: 'stylesheet',
+      href: 'https://fonts.googleapis.com/css2?family=Roboto+Mono:wght@300;400;500;600;700&display=swap',
+    },
+  ]
+}
 
 export default function App() {
   return (
