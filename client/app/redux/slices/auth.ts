1dc7bcb5e9a156af95a554b669339388c28eccf5
Danilych <danilprov2004@gmail.com>
2023-04-24 13:43:59 +0200

    feat: implement logout

--- client/app/redux/slices/auth.ts
+++ client/app/redux/slices/auth.ts
@@ -1,39 +1,48 @@
-import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
-import instance from '~/axios';
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+import instance from "~/axios";
 
-export const fetchAuth = createAsyncThunk('auth/fetchAuth', async (params: any) => {
-  const { data } = await instance.post('/login', params);
-  
-  return data;
-});
+export const fetchAuth = createAsyncThunk(
+  "auth/fetchAuth",
+  async (params: any) => {
+    const { data } = await instance.post("/login", params);
+
+    return data;
+  }
+);
 
 const initialState = {
   data: null,
-  status: 'loading',
+  status: "loading",
 };
 
 const authSlice = createSlice({
-  name: 'auth',
+  name: "auth",
   initialState,
-  reducers: {},
+  reducers: {
+    logout: (state: any) => {
+      state.data = null;
+    },
+  },
   extraReducers: {
     // @ts-ignore
     [fetchAuth.pending]: (state) => {
-      state.status = 'loading';
+      state.status = "loading";
       state.data = null;
     },
     // @ts-ignore
     [fetchAuth.fulfilled]: (state, action) => {
-      state.status = 'loaded';
+      state.status = "loaded";
       state.data = action.payload;
     },
     // @ts-ignore
     [fetchAuth.rejected]: (state) => {
-      state.status = 'error';
+      state.status = "error";
       state.data = null;
     },
   },
 });
+
+export const { logout } = authSlice.actions;
 
 export const selectIsAuth = (state: any) => Boolean(state.auth.data);
 
